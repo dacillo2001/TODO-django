46c6f5887ec73e5c98d297f5349a3688b4b0e43d
dacillo2001 <dacillonicole8@gmail.com>
2025-04-04 17:44:15 +0800

    Update ALLOWED_HOSTS to include Render domain

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%_r@)qavsrzne0pr-8h+iqfpc$sypf6xzebk9bg_-y_j6voj(o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-django-eugc.onrender.com']
 
 
 # Application definition
